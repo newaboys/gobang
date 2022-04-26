ffa9e5e051f60c2aa51e45c9fc2f59f833c0b028
mxf <269025097@qq.com>
2022-04-26 19:37:19 +0800

    修改配置文件

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ var expressWs = require('express-ws');
 var app = express();
 expressWs(app);
 var wsMap = require('./src/server/wsMap')();
-app.ws("/"+process.env.VITE_WEBSOCKET_SUFFIX, function (ws, req){
+app.ws("/"+process.env.WEBSOCKET_SUFFIX, function (ws, req){
     ws.on('message', function (msg) {
         wsMap.msgDispatcher(ws,msg);
     });
 });
-app.listen(process.env.VITE_WEBSOCKET_PORT, function () {
-    console.log('Example app listening on port '+process.env.VITE_WEBSOCKET_PORT+'!');
+app.listen(process.env.SERVER_PORT, function () {
+    console.log('app listening on port '+process.env.SERVER_PORT+'!');
 });
