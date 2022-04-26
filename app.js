dacb19b8be618516d546645b83f0ae0cc207c27d
mxf <269025097@qq.com>
2022-04-26 19:14:27 +0800

    server 去掉客户端代码

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var express = require('express');
 var expressWs = require('express-ws');
 var app = express();
 expressWs(app);
-var wsMap = require('./wsMap')();
+var wsMap = require('./src/server/wsMap')();
 app.ws("/"+process.env.VITE_WEBSOCKET_SUFFIX, function (ws, req){
     ws.on('message', function (msg) {
         wsMap.msgDispatcher(ws,msg);
